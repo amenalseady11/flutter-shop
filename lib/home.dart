bbcb87bdc81aa196487bb3596c456c4f5294d4dd
Lucas <eu@lucaspaz.com>
2019-05-08 19:06:30 -0300

    Quase la

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'styles/palette.dart';
 
 class HomeWidget extends StatefulWidget {
@@ -147,10 +148,224 @@ class _HomeState extends State<HomeWidget> {
                   ),
                 ],
               )
-            )
+            ),
+
+            new SizedBox(height: 24),
+
+            new Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  new Text("Today's Promo", style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600)),
+
+                  new Row(
+                      children: <Widget>[
+                        new Text('View all', style: TextStyle(fontSize: 12, color: Palette.purple)),
+                        new SizedBox(width: 4),
+                        new Icon(Icons.arrow_forward, size: 16, color: Palette.purple)
+                      ]
+                  )
+                ]
+            ),
+
+            SizedBox(height: 16),
+            // Today's Promo
+            Container(
+              height: 136.0,
+              transform: Matrix4.translationValues(-10,0,0),
+              child: CustomScrollView(
+                scrollDirection: Axis.horizontal,
+                shrinkWrap: false,
+                slivers: <Widget>[
+                  new SliverPadding(
+                    padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+                    sliver: new SliverList(
+                      delegate: new SliverChildBuilderDelegate(
+                        (context, index) => new Row(
+                        children: <Widget>[
+                            Container(
+                                width: 224,
+                                height: 136,
+                                margin: EdgeInsets.only(right: 16),
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(6.0),
+                                  gradient: LinearGradient(
+                                    begin: Alignment.topLeft,
+                                    end: Alignment.bottomLeft,
+                                    colors: [
+                                      const Color.fromRGBO(49, 80, 180, 1.0),
+                                      const Color.fromRGBO(15, 44, 145, 1.0)
+                                    ]
+                                  )
+                                ),
+                              ),
+                            Container(
+                                width: 224,
+                                height: 136,
+                                margin: EdgeInsets.only(right: 16),
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(6.0),
+                                    gradient: LinearGradient(
+                                        begin: Alignment.topLeft,
+                                        end: Alignment.bottomLeft,
+                                        colors: [
+                                          const Color.fromRGBO(49, 80, 180, 1.0),
+                                          const Color.fromRGBO(15, 44, 145, 1.0)
+                                        ]
+                                    )
+                                ),
+                              ),
+                            Container(
+                                width: 224,
+                                height: 136,
+                                margin: EdgeInsets.only(right: 16),
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(6.0),
+                                    gradient: LinearGradient(
+                                        begin: Alignment.topLeft,
+                                        end: Alignment.bottomLeft,
+                                        colors: [
+                                          const Color.fromRGBO(49, 80, 180, 1.0),
+                                          const Color.fromRGBO(15, 44, 145, 1.0)
+                                        ]
+                                    )
+                                ),
+                              ),
+                            Container(
+                              width: 224,
+                              height: 136,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(6.0),
+                                gradient: LinearGradient(
+                                  begin: Alignment.topLeft,
+                                  end: Alignment.bottomLeft,
+                                    colors: [
+                                      const Color.fromRGBO(49, 80, 180, 1.0),
+                                      const Color.fromRGBO(15, 44, 145, 1.0)
+                                    ]
+                                  )
+                                ),
+                              )
+                          ],
+                        ),
+                        childCount: 1
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ),
+
+            SizedBox(height: 24),
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                new Text("Trending Furniture", style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600)),
+
+                new Row(
+                  children: <Widget>[
+                    new Text('View all', style: TextStyle(fontSize: 12, color: Palette.purple)),
+                    new SizedBox(width: 4),
+                    new Icon(Icons.arrow_forward, size: 16, color: Palette.purple)
+                  ]
+                )
+              ]
+            ),
+            SizedBox(height: 24),
+
+            // Trending Furniture
+            Container(
+              height: 136.0,
+              child: CustomScrollView(
+                scrollDirection: Axis.horizontal,
+                shrinkWrap: false,
+                slivers: <Widget>[
+                  new SliverPadding(
+                    padding: const EdgeInsets.symmetric(vertical: 0.0, horizontal: 0.0),
+                    sliver: new SliverList(
+                      delegate: new SliverChildBuilderDelegate(
+                          (context, index) => new Row(
+                          children: <Widget>[
+                            Container(
+                              width: 240,
+                              height: 136,
+                              margin: EdgeInsets.only(right: 16),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(6.0),
+                                image: DecorationImage(
+                                  image: AssetImage('assets/img/trending/1.jpg'),
+                                  fit: BoxFit.fill
+                                )
+                              ),
+                            ),
+                            Container(
+                              width: 240,
+                              height: 136,
+                              margin: EdgeInsets.only(right: 16),
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(6.0),
+                                  image: DecorationImage(
+                                      image: AssetImage('assets/img/trending/2.jpg'),
+                                      fit: BoxFit.fill
+                                  )
+                              ),
+                            ),
+                            Container(
+                              width: 240,
+                              height: 136,
+                              margin: EdgeInsets.only(right: 16),
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(6.0),
+                                  image: DecorationImage(
+                                      image: AssetImage('assets/img/trending/1.jpg'),
+                                      fit: BoxFit.fill
+                                  )
+                              ),
+                            ),
+                            Container(
+                              width: 240,
+                              height: 136,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(6.0),
+                                  image: DecorationImage(
+                                      image: AssetImage('assets/img/trending/2.jpg'),
+                                      fit: BoxFit.fill
+                                  )
+                              ),
+                            )
+                          ],
+                        ),
+                        childCount: 1,
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ),
           ],
         ),
-      )
+      ),
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        notchMargin: 4.0,
+        child: new Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Container(
+              child: Icon(Icons.home),
+            ),
+
+            Container(
+              child: Icon(FontAwesomeIcons.envelope, size: 18),
+            ),
+
+            Container(
+              child: Icon(FontAwesomeIcons.box, size: 18),
+            ),
+          ]
+        )
+      ),
     );
   }
 }

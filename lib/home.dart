84610d7d8a57b28cdc2fa416a960ec43f159a543
Lucas <eu@lucaspaz.com>
2019-05-18 02:31:27 -0300

    ...

--- lib/home.dart
+++ lib/home.dart
@@ -1,7 +1,5 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import 'styles/palette.dart';
 import 'components/home/header.dart';
